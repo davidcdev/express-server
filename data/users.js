f292c3c0a92650fcfe5afb2a7ec24e7e5d313b41
David Castellanos <128877503+davidcdev@users.noreply.github.com>
2023-09-21 12:29:42 -0500

    Create a mockup of users

---
+++ data/users.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    name: "user1",
+    email: "user1@email.com",
+    password: "user1"
+  },
+  {
+    name: "user2",
+    email: "user2@email.com",
+    password: "user2"
+  },
+  {
+    name: "user3",
+    email: "user3@email.com",
+    password: "user3"
+  },
+  {
+    name: "user4",
+    email: "user4@email.com",
+    password: "user4"
+  }
+];
